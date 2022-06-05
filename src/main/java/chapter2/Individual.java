60ac1443306a424773f906939e4f2da4ca7e1d30
andrey.oshkin <vchora96@yandex.ru>
2022-06-05 19:47:12 +0300

    second commit

--- src/main/java/chapter2/Individual.java
+++ src/main/java/chapter2/Individual.java
@@ -18,32 +18,40 @@ public class Individual {
                 this.setGene(gene, 0);
             }
         }
-        public int[] getChromosome() {
-            return this.chromosome;
-        }
-        public int getChromosomeLength() {
-            return this.chromosome.length;
-        }
-        public void setGene(int offset, int gene){
-            this.chromosome[offset] = gene;
-        }
-        public int getGene(int offset){
-            return this.chromosome[offset];
-        }
-        public void setFitness(double fitness){
-            this.fitness = fitness;
-        }
-        public double getFitness () {
-            return this.fitness;
-        }
-        public String toString () {
-            String output = "";
+    }
 
-            for (int gene = 0; gene < this.chromosome.length; gene++) {
-                output += this.chromosome[gene];
-            }
-            return output;
+    public int[] getChromosome() {
+        return this.chromosome;
+    }
+
+    public int getChromosomeLength() {
+        return this.chromosome.length;
+    }
+
+    public void setGene(int offset, int gene) {
+        this.chromosome[offset] = gene;
+    }
+
+    public int getGene(int offset) {
+        return this.chromosome[offset];
+    }
+
+    public void setFitness(double fitness) {
+        this.fitness = fitness;
+    }
+
+    public double getFitness() {
+        return this.fitness;
+    }
+
+    public String toString() {
+        String output = "";
+
+        for (int gene = 0; gene < this.chromosome.length; gene++) {
+            output += this.chromosome[gene];
         }
+        return output;
     }
 }
+
 
