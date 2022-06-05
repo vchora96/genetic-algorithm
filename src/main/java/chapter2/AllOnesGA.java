372220d75f2b21746021c0de4b4b7bda713699d9
andrey.oshkin <vchora96@yandex.ru>
2022-06-05 19:40:18 +0300

    first commit

---
+++ src/main/java/chapter2/AllOnesGA.java
@@ -0,0 +1,9 @@
+package chapter2;
+
+public class AllOnesGA {
+    public static void main(String[] args) {
+        // Create GA object
+        GeneticAlgorithm ga = new GeneticAlgorithm(100, 0.01,
+                0.95, 0); // We’ll add a lot more here...
+    }
+}
---
+++ src/main/java/chapter2/GeneticAlgorithm.java
@@ -0,0 +1,22 @@
+package chapter2;
+
+/**
+ * Lots of comments in the source that are omitted here!
+ */
+public class GeneticAlgorithm {
+    private int populationSize;
+    private double mutationRate;
+    private double crossoverRate;
+    private int elitismCount;
+
+    public GeneticAlgorithm(int populationSize, double mutationRate, double
+            crossoverRate, int elitismCount) {
+        this.populationSize = populationSize;
+        this.mutationRate = mutationRate;
+        this.crossoverRate = crossoverRate;
+        this.elitismCount = elitismCount;
+    }
+    /**
+     * Many more methods implemented later...
+     */
+}
